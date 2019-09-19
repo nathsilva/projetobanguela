7c4c1f838d3c18657d07dfd41e02e6f4dd2d7196
nat <nathaliasilvanov@gmail.com>
2019-09-18 22:13:51 -0300

    ola

---
+++ notas/app/src/main/java/br/com/etecia/notas/MaindoisActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.notas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaindoisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maindois);
+    }
+}
